438945bb354d173474f166b34b564b96ba72514a
flotr18 <trouilletflorent@outlook.com>
2025-02-13 17:07:41 +0100

    Update StkEx.py

--- StkEx.py
+++ StkEx.py
@@ -1,4 +1,20 @@
-def hello():
-    print("Hello")
+import random as rd
+Stock = {}
 
-hello()
+for i in range(2010,2020):
+    Stock[i] = {
+        'CAC40' : rd.randint(1,9),
+        'NASDAQ' : rd.randint(1,9),
+        'EURONEXT' : rd.randint(1,9)
+    }
+
+    print(Stock[i]['CAC40'])
+    print(Stock[i]['NASDAQ'])
+
+
+    if Stock[i]['CAC40'] > Stock[i]['NASDAQ']:
+        print("C")
+    else:
+        print("W")
+
+    
